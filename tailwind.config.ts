0e4f2de219e12a091c631fc50356219c258ee913
Javier Reyes <javier.reys@gmail.com>
2023-07-26 05:10:52 -0400

    feat: add new font to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "X",
+          {
+            ...defaultTheme.fontFamily.sans,
+            fontFeatureSettings: '"ss04"',
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
